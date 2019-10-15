022ab69f410fb75150a939aae556a4dd0a4b647f
Rahul Wadhwani <rahulwadhwani21@gmail.com>
2019-10-15 23:10:09 +0100

    Created tests for binary tree

---
+++ tests/binary_tree/binary_tree_test.cc
@@ -0,0 +1,36 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "src/binary_tree/binary_tree.h"
+
+TEST(BinaryTree, SmallTiltTree) {
+    binarytree bt;
+    bt.insertNode(10);
+    bt.insertNode(8);
+    bt.insertNode(12);
+    ASSERT_EQ(4, bt.tiltTree());
+}
+
+TEST(BinaryTree, OneBranchTreeTilt) {
+    binarytree leftBranchBt;
+    leftBranchBt.insertNode(10);
+    leftBranchBt.insertNode(8);
+
+    binarytree rightBranchBt;
+    rightBranchBt.insertNode(10);
+    rightBranchBt.insertNode(12);
+
+    ASSERT_EQ(8, leftBranchBt.tiltTree());
+    ASSERT_EQ(12, rightBranchBt.tiltTree());
+}
+
+TEST(BinaryTree, MediumTiltTree) {
+    binarytree bt;
+    bt.insertNode(50);
+    bt.insertNode(30);
+    bt.insertNode(70);
+    bt.insertNode(10);
+    bt.insertNode(20);
+    bt.insertNode(60);
+    bt.insertNode(90);
+    ASSERT_EQ(80, bt.tiltTree());
+}
