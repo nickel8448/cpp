0add66c645cbb815c80ede981405b17545729338
Rahul Wadhwani <rahulwadhwani21@gmail.com>
2020-02-17 22:21:40 +0000

    Testing create vector function

--- src/cs106l/move_semantics/move_vector.cc
+++ src/cs106l/move_semantics/move_vector.cc
@@ -2,6 +2,13 @@
 #include <iostream>
 #include <algorithm>
 
+
+std::vector<int> CreateVector() {
+  std::vector<int> temp{1, 2, 3, 4, 5};
+  return temp;
+}
+
+
 int main() {
   std::vector<int> v1{1, 2, 3, 4, 5};
   std::vector<int> v2;
@@ -11,7 +18,6 @@ int main() {
   std::cout << "Size of v2: " << v2.size() << std::endl;
   std::cout << "Vector size " << v1.size() << std::endl;
 
-  std::cout <<
 
   // Moving v1 to v2
   v2 = std::move(v1);
@@ -19,4 +25,11 @@ int main() {
   std::cout << "After moving" << std::endl;
   std::cout << "Size of v1: " << v1.size() << std::endl;
   std::cout << "Size of v2: " << v2.size() << std::endl;
+
+  std::cout << "Creating a vector from a function and storing it in main";
+  std::cout << std::endl;
+  std::vector<int> v3;
+  v3 = CreateVector();
+  std::cout << "Size of new vector" << std::endl;
+  std::cout << v3.size() << std::endl;
 }
